9908748dc62f04aeccf69dcc7233da7e6c1d711f
Austen Haymond <ahaymon17@gmail.com>
2020-06-09 17:23:05 -0700

    fixed image components

--- client/src/components/carousel.js
+++ client/src/components/carousel.js
@@ -3,12 +3,12 @@ import React from "react";
 import "../assets/styles/image-gallery.css";
 import ImageGallery from 'react-image-gallery';
 
-import example1 from "../assets/images/example1.JPG";
-import example2 from "../assets/images/example2.JPG";
-import example3 from "../assets/images/example3.JPG";
-import example4 from "../assets/images/example4.JPG";
-import example5 from "../assets/images/example5.JPG";
-import example6 from "../assets/images/example6.JPG";
+import example1 from "../assets/images/example1.jpg";
+import example2 from "../assets/images/example2.jpg";
+import example3 from "../assets/images/example3.jpg";
+import example4 from "../assets/images/example4.jpg";
+import example5 from "../assets/images/example5.jpg";
+import example6 from "../assets/images/example6.jpg";
 
 class Carousel extends React.Component {
 
