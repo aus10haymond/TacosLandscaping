3b397c01a5040f7b517a590fdf564ee0f3c494dc
Austen Haymond <ahaymon17@gmail.com>
2020-06-09 17:50:04 -0700

    moved example photos

--- client/src/components/carousel.js
+++ client/src/components/carousel.js
@@ -3,12 +3,12 @@ import React from "react";
 import "../assets/styles/image-gallery.css";
 import ImageGallery from 'react-image-gallery';
 
-import example1 from "../assets/images/example1.jpg";
-import example2 from "../assets/images/example2.jpg";
-import example3 from "../assets/images/example3.jpg";
-import example4 from "../assets/images/example4.jpg";
-import example5 from "../assets/images/example5.jpg";
-import example6 from "../assets/images/example6.jpg";
+import example1 from "../assets/images/examples/example1.jpg";
+import example2 from "../assets/images/examples/example2.jpg";
+import example3 from "../assets/images/examples/example3.jpg";
+import example4 from "../assets/images/examples/example4.jpg";
+import example5 from "../assets/images/examples/example5.jpg";
+import example6 from "../assets/images/examples/example6.jpg";
 
 
 class Carousel extends React.Component {
