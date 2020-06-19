1bec7f76817d74f7aa483ac5880290155623f4dc
Austen Haymond <ahaymon17@gmail.com>
2020-06-18 17:18:45 -0700

    fixed spelling error in spanish about section

--- client/src/components/spanish/spanishAbout.js
+++ client/src/components/spanish/spanishAbout.js
@@ -7,7 +7,7 @@ function SpanishAbout() {
             <br />
             <div id="aboutUs" className="card">
                 <p>
-                    Taco's Landscaping es un pequeño negocio familiar que ha estado al servicio de la gente de Arizona por 20 años. Somos un equipo de padre e hijo. ¡A Eustaquio le conocen como "Taco" porque es más facil de decir! Con todo su habilidad de jardinería, Taco hará el trabajo. José se encarga de las tareas administrativas.
+                    Taco's Landscaping es un pequeño negocio familiar que ha estado al servicio de la gente de Arizona por 20 años. Somos un equipo de padre e hijo. ¡A Eustaquio le conocen como "Taco" porque es más facil de decir! Con toda su habilidad de jardinería, Taco hará el trabajo. José se encarga de las tareas administrativas.
                 </p>
                 <p>
                     Juntos estamos comprometidos de proveer la mejor calidad por un precio justo, ayudando a nuestros clientes a embellecer sus casas y vidas. Nos encanta la jardinería, dejando al mundo en un estado mejor y más bello que cómo lo encontramos. ¡Estamos emocionados por tener la oportunidad de trabajar con usted y llevar a cabo su visión!
